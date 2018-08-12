cdd3c5bb882e4fd4f04bdc168fe2e93cf95d1526
Abel <AbelTeferaNY@gmail.com>
2018-08-12 03:12:26 +0000

    added sample json input and response for referenc (sample_data.json)

--- start_server.bash
+++ start_server.bash
@@ -2,5 +2,6 @@
 source ./venv/bin/activate
 #load configuration file
 source ./configuration.sh
-#start server
-flask run --host=0.0.0.0 --port=$FLASK_PORT
+#start server in the background
+#flask run --host=0.0.0.0 --port=$FLASK_PORT
+uwsgi --http :$FLASK_PORT --wsgi-file wsgi.py --master --processes 4 --threads 2 &
