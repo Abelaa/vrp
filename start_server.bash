3d952fe3fbe9e3eaa07374da4d9eea69da7be1b5
root <root@centos-app.c.pure-pact-214108.internal>
2018-09-06 07:57:36 +0000

    nothing

--- start_server.bash
+++ start_server.bash
@@ -4,5 +4,5 @@ source ./configuration.sh
 #enable python3 on centos linux
 #scl enable rh-python36 bash
 #run production server
-uwsgi --http-socket 0.0.0.0:$FLASK_PORT --wsgi-file wsgi.py --master --processes $NUM_PROCESSES --threads $NUM_THREADS --safe-pidfile pid_list.txt &
-#uwsgi --socket 0.0.0.0:6000 --protocol=http -w wsgi &
+nohup uwsgi --http-socket 0.0.0.0:$FLASK_PORT --wsgi-file wsgi.py --master --processes $NUM_PROCESSES --threads $NUM_THREADS --safe-pidfile pid_list.txt &
+
